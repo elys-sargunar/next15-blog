4dda2a0497c64593ea0cb5d44752838f1167d6fb
Elys Sargunar <elys.sargunar@tpximpact.com>
2025-03-22 14:19:26 +0000

    feature: remove posts.

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
-import PostCard from "@/components/PostCard";
-import { getCollection } from "@/lib/db";
-
 export default async function Home() {
-
-  const postsCollection = await getCollection("posts")
-  const posts = await postsCollection?.find().sort({$natural: -1}).toArray()
-
-  // console.log(posts)
-
-  if(posts){
-    return (
-      <div className="grid grid-cols-2 gap-6">
-        {
-          posts.map((post) => (
-            <div key={post._id.toString()}>
-              <PostCard post={post as unknown as {_id: string, title: string, content: string}} ></PostCard>
-            </div>
-          ))
-        }
-      </div>
-    )
-  }
-  else {
-    return <p>Cannot find any posts.</p>
-  }
+return (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+)
 }
--- app/posts/create/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default async function create(){
-    return (
-        <div className="container w-1/2">
-            <h1 className="title">Create a new post</h1>
-        </div>
-    )
-}
