249b7c1f4cd74e753f98ea0815bde758156a1249
Elys Sargunar <elys@sargunar.com>
2025-03-27 11:55:53 +0000

    lint: cont.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { getUserProfile } from "@/actions/auth";
 import { OrderItem, ClientOrder, UserReference } from "@/lib/rules";
 
